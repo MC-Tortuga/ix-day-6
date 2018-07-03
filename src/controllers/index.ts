89e4a518ec6d5e714cf4f63e3c0ecb63feb97bdd
MC-Tortuga <malcolmcarelse@gmail.com>
2018-07-03 16:27:18 +0200

    removed unnecessary file

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,4 +2,3 @@ export * from './ping.controller';
 export * from './registration.controller';
 export * from './login.controller';
 export * from './users.controller';
-export * from './auth.controller';
